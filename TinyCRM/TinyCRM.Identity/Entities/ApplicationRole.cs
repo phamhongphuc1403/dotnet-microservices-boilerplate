be541f6bcb581c4fceadc216caedabb4365b4fb5
phamhongphuc1403 <phamhongphuc1403@gmail.com>
2023-09-17 01:57:05 +0700

    chore: remove old monolithic project

--- TinyCRM/TinyCRM.Identity/Entities/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TinyCRM.Identity.Entities;
-
-public class ApplicationRole : IdentityRole
-{
-    public virtual ICollection<IdentityRoleClaim<string>> Claims { get; set; } =
-        new HashSet<IdentityRoleClaim<string>>();
-}
--- TinyCRM/TinyCRM.Identity/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TinyCRM.Identity.Entities;
-
-public class ApplicationUser : IdentityUser
-{
-    public string? Name { get; set; }
-    public string? RefreshToken { get; set; }
-}
