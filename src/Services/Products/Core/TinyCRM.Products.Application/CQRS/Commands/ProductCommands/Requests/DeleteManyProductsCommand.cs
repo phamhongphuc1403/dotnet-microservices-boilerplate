48820a44b1bbf619d7fa8ab8bd7f114a3cb5f50b
phamhongphuc1403 <phamhongphuc1403@gmail.com>
2023-10-07 10:56:20 +0700

    feat: delete many products

---
+++ src/Services/Products/Core/TinyCRM.Products.Application/CQRS/Commands/ProductCommands/Requests/DeleteManyProductsCommand.cs
@@ -0,0 +1,12 @@
+using BuildingBlock.Application.CQRS;
+using TinyCRM.Products.Application.DTOs;
+
+namespace TinyCRM.Products.Application.CQRS.Commands.ProductCommands.Requests;
+
+public class DeleteManyProductsCommand : DeleteManyProductsDto, ICommand
+{
+    public DeleteManyProductsCommand(DeleteManyProductsDto dto)
+    {
+        Ids = dto.Ids;
+    }
+}
