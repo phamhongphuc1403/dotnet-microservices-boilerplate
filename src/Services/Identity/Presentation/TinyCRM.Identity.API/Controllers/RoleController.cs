a8b256cf07fdb79254aebe6fc4e76d0f9c4910f8
phamhongphuc1403 <phamhongphuc1403@gmail.com>
2023-10-14 08:58:39 +0700

    feat: get all roles

--- src/Services/Identity/Presentation/TinyCRM.Identity.API/Controllers/RoleController.cs
+++ src/Services/Identity/Presentation/TinyCRM.Identity.API/Controllers/RoleController.cs
@@ -1,6 +1,9 @@
+using BuildingBlock.Domain.Constants.Identity;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TinyCRM.Identity.Application.CQRS.Commands.Requests;
+using TinyCRM.Identity.Application.CQRS.Queries.RoleQueries.Requests;
 using TinyCRM.Identity.Application.DTOs.RoleDTOs;
 
 namespace Identities.API.Controllers;
@@ -17,10 +20,20 @@ public class RoleController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Policy = Permissions.Role.Create)]
     public async Task<ActionResult<RoleDto>> Login(CreateOrEditRoleDto dto)
     {
         var response = await _mediator.Send(new CreateRoleCommand(dto));
 
         return Ok(response);
+    }
+
+    [HttpGet]
+    [Authorize(Policy = Permissions.Role.View)]
+    public async Task<ActionResult<IEnumerable<RoleDto>>> GetAllRoles()
+    {
+        var roles = await _mediator.Send(new GetAllRolesQuery());
+
+        return Ok(roles);
     }
 }
